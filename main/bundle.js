7d0a7f3bf5fbc83d15b5f60b9fdafda6d230dad7
Welson Júnior <welsongt9@live.com>
2020-08-03 23:01:54 -0300

    Manipulation of array

--- main/bundle.js
+++ main/bundle.js
@@ -1,21 +1,27 @@
 "use strict";
 
-var a = 1; // Não pode ter o valor reatribuido
-//a = 3 // Não é possível
-//Mas, a mutação é possivel. Veja o exemplo abaixo.
+var arr = [1, 3, 4, 5, 9, 8, 7]; //Map
+//Percorre o vetor e executa a ação desejada
 
-var usuario = {
-  nome: 'Diego'
-};
-usuario.nome = 'Cleiton';
-console.log(usuario);
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr); //Reduce
+//Pega tudo e transforma em uma única informação 
 
-function teste(x) {
-  var y = 2; //let variável escopo
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum); //Serve para filtrar o array, tipo pegar só os pares ou apenas os impares
 
-  if (x > 5) {
-    console.log(x, y);
-  }
-}
+var filter = arr.filter(function (item) {
+  //Ela obrigatóriamente precisa retornar true ou false  
+  return item % 2 === 0;
+});
+console.log(filter); //Find
+//Verificar uma informação dentro de um array
 
-teste(10);
+var find = arr.find(function (item) {
+  return item === 4;
+});
+console.log(find);
--- main/main.js
+++ main/main.js
@@ -1,18 +1,33 @@
-const a = 1 // Não pode ter o valor reatribuido
-//a = 3 // Não é possível
+const arr = [1,3,4,5,9,8,7]
 
-//Mas, a mutação é possivel. Veja o exemplo abaixo.
-const usuario = {nome:'Diego'}
-usuario.nome='Cleiton'
-console.log(usuario)
+//Map
+//Percorre o vetor e executa a ação desejada
+const newArr = arr.map(function(item, index){
+    return item + index
+})
 
-function teste(x){
-    let y = 2 //let variável escopo
+console.log(newArr)
 
-    if(x>5){
-        console.log(x, y)
-    }
+//Reduce
+//Pega tudo e transforma em uma única informação 
+const sum = arr.reduce(function(total, next){
+    return total+next
+})
 
-}
+console.log(sum)
 
-teste(10)
+//Serve para filtrar o array, tipo pegar só os pares ou apenas os impares
+const filter = arr.filter(function(item){
+//Ela obrigatóriamente precisa retornar true ou false  
+    return item % 2 === 0
+})
+
+console.log(filter)
+
+//Find
+//Verificar uma informação dentro de um array
+const find = arr.find(function(item){
+    return item === 4
+})
+
+console.log(find)
