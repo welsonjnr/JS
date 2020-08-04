4109e5b42903dfca642104f6a40fd790a111f9e1
Welson Júnior <welsongt9@live.com>
2020-08-03 22:48:59 -0300

    Variables JS

--- main/main.js
+++ main/main.js
@@ -1,9 +1,18 @@
-class Matematica{
-//Método estático não consegue ver o restante da classe, 
-//ocasionando a não utilização do constructor
-    static soma(a, b){
-        return a + b
+const a = 1 // Não pode ter o valor reatribuido
+//a = 3 // Não é possível
+
+//Mas, a mutação é possivel. Veja o exemplo abaixo.
+const usuario = {nome:'Diego'}
+usuario.nome='Cleiton'
+console.log(usuario)
+
+function teste(x){
+    let y = 2 //let variável escopo
+
+    if(x>5){
+        console.log(x, y)
     }
+
 }
 
-console.log(Matematica.soma(1, 2))
+teste(10)
