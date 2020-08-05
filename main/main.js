ce159386a149388f7ff7e55ebca978537974987d
Welson Júnior <welsongt9@live.com>
2020-08-05 01:08:55 -0300

    SPREAD and REST

--- main/main.js
+++ main/main.js
@@ -1,22 +1,55 @@
+//REST
+//Pegar o resto da propriedades
+
 const usuario = {
-    nome:'Joao',
-    idade: 23,
-    endereco:{
-        cidade: 'Rio do Sul',
-        estado: 'SC',
-    },
+    nome: 'Joao',
+    idade: 21,
+    empresa: 'Cleitionho'
 }
 
-console.log(usuario)
-
-const { nome, idade , endereco:{cidade}} = usuario;
+//Esse ponto vai armazenar o restante das informações em outra variável
+const {nome, ...resto} = usuario
 
 console.log(nome)
-console.log(idade)
-console.log(cidade)
+console.log(resto)
 
-function mostraNome({nome, idade}){
-    console.log(nome, idade)
+const arr = [1,2,3,4]
+
+const [a, b, ...c] = arr
+
+console.log(a)
+console.log(c)
+
+//REST para parâmetros
+function soma(...params){
+    return params.reduce((total, next) => total + next) 
 }
 
-mostraNome(usuario)
+console.log(soma(1, 3, 4))
+
+function sum(a, b, ...params){
+    return params
+}
+
+console.log(sum(1, 3, 4))
+
+//SPREAD
+//Repassa as informações adquiridas para outra estrutura de dados
+const arr1 = [1,2,3]
+const arr2 = [4,5,6]
+
+const arr3 = [...arr1, ...arr2]
+
+console.log(arr3)
+
+
+const usuario1 = {
+    nome: 'Paulo',
+    idade: 12,
+    empresa: 'Pamonharia'
+}
+
+//Recebe todas as variáveis e informação do usuário1 e muda o nome
+const usuario2 = {...usuario1, nome:'Gabriel'}
+
+console.log(usuario2)
